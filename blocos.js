0da43851f5bc66fb28686706c08d3c3392e70f1b
Carollina Gillio <109316837+carolinagillio@users.noreply.github.com>
2022-09-02 18:58:28 -0300

    desafio

---
+++ blocos.js
@@ -0,0 +1,616 @@
+var blocos = [
+    {
+       "unidade":"122",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"172",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"64",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"71",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"131",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"63",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"172",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"43",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"151",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"113",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"14",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"103",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"114",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"73",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"143",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"123",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"174",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"182",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"92",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"183",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"162",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"92",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"152",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"153",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"82",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"104",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"173",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"102",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"82",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"83",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"11",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"84",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"122",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"31",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"123",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"191",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"183",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"192",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"132",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"61",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"81",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"32",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"193",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"41",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"71",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"11",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"104",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"13",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"164",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"72",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"62",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"41",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"124",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"153",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"184",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"53",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"73",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"144",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"42",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"134",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"154",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"142",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"141",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"81",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"91",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"163",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"93",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"62",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"103",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"133",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"91",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"61",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"51",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"23",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"34",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"182",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"21",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"44",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"94",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"101",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"22",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"164",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"64",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"151",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"114",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"152",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"33",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"171",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"143",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"121",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"22",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"171",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"34",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"194",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"112",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"84",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"23",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"24",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"13",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"112",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"33",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"131",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"54",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"141",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"101",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"142",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"52",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"161",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"74",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"53",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"72",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"134",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"14",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"173",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"83",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"162",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"161",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"181",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"184",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"144",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"44",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"163",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"102",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"154",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"121",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"111",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"113",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"74",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"93",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"52",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"42",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"181",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"133",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"111",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"51",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"54",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"124",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"21",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"94",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"31",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"24",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"132",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"12",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"00",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"43",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"63",
+       "bloco":"Bloco A"
+    },
+    {
+       "unidade":"32",
+       "bloco":"Bloco B"
+    },
+    {
+       "unidade":"174",
+       "bloco":"Bloco B"
+    }
+ ];
+
+blocos.sort(function(a, b) {
+    return a.unidade  -  b.unidade;
+}); 
+
+BlocoA = Array();
+BlocoB = Array();
+
+for (let index = 0; index < blocos.length; index++) {
+
+    if(blocos[index]['bloco'] == 'Bloco B'){
+        BlocoB.push(blocos[index]) 
+    }
+
+    if(blocos[index]['bloco'] == 'Bloco A'){
+        BlocoA.push(blocos[index]) 
+    }
+}  
+
+BlocosOrdenados = BlocoA.concat(BlocoB);
+  
+console.log(BlocosOrdenados)
